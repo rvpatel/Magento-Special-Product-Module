57a802c10e5bab9a4d14f50d2052ce8f725264bb
Ravi Patel <ravipatelb07272@gmail.com>
2015-03-26 10:28:27 +0530

    Create Sales.php

---
+++ app/code/local/PWS/Special/Block/Catalog/Product/Sales.php
@@ -0,0 +1,38 @@
+<?php 
+class PWS_Special_Block_Catalog_Product_Sales extends Mage_Catalog_Block_Product_List{
+    protected function _getProductCollection(){
+        if (is_null($this->_productCollection)) {
+            $todayStartOfDayDate  = Mage::app()->getLocale()->date()
+                ->setTime('00:00:00')
+                ->toString(Varien_Date::DATETIME_INTERNAL_FORMAT);
+
+            $todayEndOfDayDate  = Mage::app()->getLocale()->date()
+                ->setTime('23:59:59')
+                ->toString(Varien_Date::DATETIME_INTERNAL_FORMAT);
+
+            $collection = Mage::getResourceModel('catalog/product_collection');
+            $collection->setVisibility(Mage::getSingleton('catalog/product_visibility')->getVisibleInCatalogIds());
+
+
+            $collection = $this->_addProductAttributesAndPrices($collection)
+                ->addStoreFilter()
+                ->addAttributeToFilter('special_from_date', array('or'=> array(
+                    0 => array('date' => true, 'to' => $todayEndOfDayDate),
+                    1 => array('is' => new Zend_Db_Expr('null')))
+                ), 'left')
+                ->addAttributeToFilter('special_to_date', array('or'=> array(
+                    0 => array('date' => true, 'from' => $todayStartOfDayDate),
+                    1 => array('is' => new Zend_Db_Expr('null')))
+                ), 'left')
+                ->addAttributeToFilter(
+                    array(
+                        array('attribute' => 'special_from_date', 'is'=>new Zend_Db_Expr('not null')),
+                        array('attribute' => 'special_to_date', 'is'=>new Zend_Db_Expr('not null'))
+                        )
+                  )
+            ;
+            $this->_productCollection = $collection;
+        }
+        return $this->_productCollection;
+    }
+}
