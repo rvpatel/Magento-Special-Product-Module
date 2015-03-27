6719acdd4d885e3dc63e5169a38dcdaa5a94db3e
Rajeev K Tomy <rajeevphpdeveloper@gmail.com>
2015-03-27 11:28:18 +0530

    Fixed errors in the module

--- app/code/local/PWS/Special/Block/Catalog/Product/List.php
+++ app/code/local/PWS/Special/Block/Catalog/Product/List.php
@@ -1,6 +1,8 @@
 <?php 
-class PWS_Special_Block_Catalog_Product_Sales extends Mage_Catalog_Block_Product_List{
-    protected function _getProductCollection(){
+class PWS_Special_Block_Catalog_Product_List extends Mage_Catalog_Block_Product_List
+{
+    protected function _getProductCollection()
+    {
         if (is_null($this->_productCollection)) {
             $todayStartOfDayDate  = Mage::app()->getLocale()->date()
                 ->setTime('00:00:00')
---
+++ app/code/local/PWS/Special/Helper/Data.php
@@ -0,0 +1,4 @@
+<?php
+class PWS_Special_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
